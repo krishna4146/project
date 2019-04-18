b3095b539207e54b2a758bb93afbff2a6629b928
krishna4146 <49722401+krishna4146@users.noreply.github.com>
2019-04-18 16:51:21 +0530

    Add files via upload

---
+++ oscode final.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+main()
+{
+	int timer=0,i=0,bt=0;
+	int process[4]={1,2,3,4};
+	int arrival[4]={0,5,13,17};
+	int burst_time[4]={20,36,19,42};
+	int burst[4]={20,36,19,42};
+	int process_arrived[4]={1};
+	double priority[4]={0};
+	int complete[4]={0};
+	while(1)
+	{
+		int k;
+		for(k=0;k<4;k++)
+		{
+			if(timer>=arrival[k] ) 
+			{
+				priority[k]=1+((timer-arrival[k])/burst[k]);
+			}
+		}
+		int p,q;
+		for(p=0;p<4;p++)
+		{
+			for(q=0;q<4-1-p;q++)
+			{
+				if(priority[q]<priority[q+1] && process_arrived[q]!=0 && process_arrived[q+1]!=0)
+				{
+					double t1=priority[q];
+					priority[q]=priority[q+1];
+					priority[q+1]=t1;
+					int t2=process_arrived[q];
+					process_arrived[q]=process_arrived[q+1];
+					process_arrived[q+1]=t2;
+				}
+				else if(priority[q]==priority[q+1]  && process_arrived[q]!=0 && process_arrived[q+1]!=0)
+				{
+					if(burst[q]<burst[q+1])
+					{
+						int t=process_arrived[q];
+						process_arrived[q]=process_arrived[q+1];
+						process_arrived[q+1]=t;
+					}
+				}
+			}
+		}
+		int y=process_arrived[0];
+		bt=burst[y-1];
+		do
+		{
+			printf("timer : %d",timer);
+			int x;
+			for(x=0;x<4;x++)
+			{
+				if(timer==arrival[x])
+				{		
+					process_arrived[x]=x+1;
+					printf("\nprocess %d arrived\n",x);
+				}	
+			}
+			bt=bt-1;
+			timer=timer+1;
+			if(bt==0)
+			{
+				printf("\nprocess %d completed\n",y );
+				complete[y-1]=timer;
+				i=i+1;
+				burst[y-1]=-1;
+				//priority[0]=0;
+				for(x=0;x<4;x++)
+				{
+					priority[x]=0;
+					if(process_arrived[x]!=0)
+					{
+						process_arrived[x]=x+1;
+					}
+				}
+				break;
+			}
+		}while(1);
+		if(i==4)
+			break;
+			
+
+	}
+	
+	int tat[5],wt[5];
+	float avg_tat,avg_wt;
+	for(i=0;i<4;i++){
+	
+	tat[i]=(complete[i]-arrival[i]);
+	wt[i]=tat[i]-burst_time[i];
+	printf("tat is %d\n",tat[i]);
+	printf("wt is %d\n",wt[i]);
+	
+	}
+	avg_tat=(tat[0]+tat[1]+tat[2]+tat[3])/4;
+	avg_wt=(wt[0]+wt[1]+wt[2]+wt[3])/4;
+	printf("average tat is %f",avg_tat);
+	printf("average waiting time is %f\n",avg_wt);
+}
+
